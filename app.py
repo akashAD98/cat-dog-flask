a6d0e0c6a8f3453d1b969ee50b833ab5bfda9f1d
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2020-10-03 02:45:29 -0700

    Update app.py
    did some hyperparameter tunnig ,changed optimizer="adam"

--- app.py
+++ app.py
@@ -8,17 +8,13 @@ import numpy as np
 model = Sequential()
 
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128,128,3)))
-model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+
 
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
@@ -27,7 +23,7 @@ model.add(Dense(512, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dropout(0.5))
 model.add(Dense(2, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.load_weights('static/model.h5')
 
 
